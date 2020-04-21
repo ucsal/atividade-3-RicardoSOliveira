b8bbf22c3f8df9e0f6ae3b61677374cb68e2c9be
Ricardo <RicardoSOliveira>
2020-04-20 21:14:56 -0300

    Atividade3

--- app/src/main/java/br/com/mariojp/exercise3/Tarefa.java
+++ app/src/main/java/br/com/mariojp/exercise3/Tarefa.java
@@ -10,10 +10,39 @@ public class Tarefa {
     }
 
     public String getDescricao() {
+
         return descricao;
     }
 
     public int getPrioridade() {
+
+
         return prioridade;
     }
+
+    // verificar se a descrição já existe
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Tarefa)) {
+            return false;
+        }
+        final Tarefa other = (Tarefa) obj;
+        return this.getDescricao().equals(other.getDescricao());
+    }
+
+    @Override
+    public String toString() {
+
+        return descricao + "  " + "Prioridade: " + prioridade;
+    }
+
+    @Override
+    public int compareTo(Tarefa tarefa) {
+        if (tarefa == null ) {
+            return 0;
+        }
+        return prioridade.compareTo(tarefa.getPrioridade());
+    }
+
+
 }
